c5e9d2d1a8ffe2a9078ab25c8ff5a8ad49c71a49
Andy Dierker <dierker@gmail.com>
2021-06-02 00:07:04 -0700

    layout and colors

---
+++ src/icons/Icons.tsx
@@ -0,0 +1,33 @@
+interface IconProps {
+  className?: string
+}
+
+export const Lock = ({className}: IconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 ${className}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+  </svg>
+)
+
+export const Unlock = ({className}: IconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 ${className}`} viewBox="0 0 20 20" fill="currentColor">
+    <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
+  </svg>
+)
+
+export const PlainX = ({className}: IconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 ${className}`} viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+  </svg>
+)
+
+export const CircleX = ({className}: IconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 ${className}`} viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+  </svg>
+)
+
+export const ArrowLeft = ({className}: IconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 ${className}`} viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clip-rule="evenodd" />
+  </svg>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,22 @@
 import Head from 'next/head'
-
-const Box = ({color}) => {
-  return (
-    <div className={`${color} h-44 w-44 flex flex-col items-center justify-center text-drkr-white`}>
-      <h1 className="text-3xl headline">
-        head
-      </h1>
-      <p className="body">body</p>
-    </div>
-  )
-}
+import {Lock} from 'icons/Icons'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>andydierker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center text-drkr-black">
-        <h1 className="text-6xl headline">
-          andydierker dot com
-        </h1>
-        <p className="body">and this is some body text</p>
-        <div className="flex flex-row">
-          <Box color={'bg-drkr-red'}/>
-          <Box color={'bg-drkr-green'}/>
-          <Box color={'bg-drkr-dark-green'}/>
-          <Box color={'bg-drkr-yellow'}/>
-          <Box color={'bg-drkr-orange'}/>
+      <main className="flex flex-col items-center justify-between w-full flex-1 px-20 text-drkr-black">
+        <div className="text-right mt-12 text-6xl lg:text-8xl headline">
+          <h1 className="text-drkr-green">andy</h1>
+          <h1 className="text-drkr-dark-green">dierker</h1>
+          <h1 className="text-drkr-yellow">dot</h1>
+          <h1 className="text-drkr-orange">com</h1>
         </div>
-
+        <Lock className="text-drkr-green h-14 w-14 lg:h-24 lg:w-24 mb-12 lg:mb-16"/>
       </main>
     </div>
   )
