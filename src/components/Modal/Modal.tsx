287454a07d42c62ba146f09089cf939d2642c5ff
Andy Dierker <dierker@gmail.com>
2021-06-28 22:40:40 -0700

    wip modal changes

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,8 +1,6 @@
 import {useRef, Fragment} from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 
-import {PlainX} from 'icons/Icons'
-
 interface ModalProps {
   isOpen: boolean
   closeModal: () => any
@@ -12,7 +10,6 @@ const Modal = ({
   isOpen,
   closeModal
 }: ModalProps) => {
-  const cancelButtonRef = useRef(null)
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
@@ -20,7 +17,6 @@ const Modal = ({
         as="div"
         static
         className="fixed z-10 inset-0 overflow-y-auto"
-        initialFocus={cancelButtonRef}
         open={isOpen}
         onClose={closeModal}
       >
@@ -34,7 +30,7 @@ const Modal = ({
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className="fixed inset-0 bg-drkr-dark-green bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
           {/* This element is to trick the browser into centering the modal contents. */}
@@ -50,40 +46,40 @@ const Modal = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <PlainX/>
-                  </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                      Deactivate account
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Are you sure you want to deactivate your account? All of your data will be permanently removed.
-                        This action cannot be undone.
-                      </p>
-                    </div>
+            <div className="inline-block align-bottom bg-drkr-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-drkr-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="mt-3 sm:mt-0 sm:ml-4 sm:text-left">
+                  <Dialog.Title as="h3" className="text-2xl leading-6 headline-spaced text-drkr-dark-green">
+                    hi, i'm a frontend dev
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-drkr-dark-green body">
+                      i use this website for stupid little personal projects.
+                    </p>
                   </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <div className="bg-drkr-white px-4 py-3 sm:px-6 sm:flex sm:flex-row justify-end">
                 <button
                   type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-drkr-yellow text-base font-medium text-drkr-white hover:bg-drkr-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-drkr-yellow sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={closeModal}
                 >
-                  Deactivate
+                  my linkedin
                 </button>
                 <button
                   type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-drkr-green text-base font-medium text-drkr-white hover:bg-drkr-green focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-drkr-green sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={closeModal}
-                  ref={cancelButtonRef}
                 >
-                  Cancel
+                  admin section
+                </button>
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-drkr-orange text-base font-medium text-drkr-white hover:bg-drkr-orange focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-drkr-orange sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={closeModal}
+                >
+                  okay, bye bye
                 </button>
               </div>
             </div>
@@ -91,19 +87,6 @@ const Modal = ({
         </div>
       </Dialog>
     </Transition.Root>
-    // <div className={`${isOpen ? 'flex' : 'hidden'} container h-full items-center justify-center absolute`}>
-    //   <div className="w-24 h-24 bg-drkr-white">
-    //     <div className="p-4 flex flex-row items-center justify-between">
-    //       <div className="headline">
-    //         {title}
-    //       </div>
-    //       <PlainX className="drkr-red" onClick={closeModal}/>
-    //     </div>
-    //     <div className="p-4">
-    //       {children}
-    //     </div>
-    //   </div>
-    // </div>
   )
 }
 
