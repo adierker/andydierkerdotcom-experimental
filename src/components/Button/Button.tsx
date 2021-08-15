b2e3b6fba6fe49d8b932aa058ae352e7470baa15
Andy Dierker <dierker@gmail.com>
2021-08-15 01:21:09 -0700

    updated button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,41 @@
+import classnames from 'classnames'
+
 interface ButtonProps {
   text: string
+  onClick: () => void
+  color?: string
+  customClasses?: string
 }
 
-export const Button ({text}: ButtonProps) => {
+const defaults = {
+  flex: 'inline-flex justify-center',
+  border: 'rounded-md border border-transparent shadow-sm',
+  focus: 'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-drkr-green',
+  padding: 'px-4 py-2',
+  typography: 'text-xl font-medium',
+  colors: 'bg-drkr-green text-drkr-white',
+  hover: 'hover:bg-opacity-75',
+}
+
+const defaultStyles = Object.values(defaults).join(' ')
+
+export const Button = ({
+  text,
+  onClick,
+  color,
+  customClasses,
+}: ButtonProps) => {
   return (
-    <div>
+    <button
+      type="button"
+      className={classnames(
+        defaultStyles,
+        color,
+        customClasses
+      )}
+      onClick={onClick}
+    >
       {text}
-    </div>
+    </button>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import {Lock} from 'icons/Icons'
 import {Modal} from 'components/Modal/Modal'
 import {useModal} from 'components/Modal/Modal.hooks'
+import {Button} from 'components/Button/Button'
 
 export default function Home() {
   const {openModal, isOpen, closeModal} = useModal()
@@ -21,6 +22,9 @@ export default function Home() {
           <h1 className="text-drkr-yellow">dot</h1>
           <h1 className="text-drkr-orange">com</h1>
         </div>
+
+        <Button text="hello" onClick={() => null}/>
+
         <Lock 
           onClick={openModal}
           className="text-drkr-green h-14 w-14 lg:h-24 lg:w-24 mb-12 lg:mb-16 cursor-pointer"
