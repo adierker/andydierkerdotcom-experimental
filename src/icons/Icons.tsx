c5684232090c59f1209c3cddbaf3b162da46f158
Andy Dierker <dierker@gmail.com>
2021-08-17 00:18:11 -0700

    add socials, update styles and spacing

--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -3,6 +3,7 @@ interface IconProps {
   onClick?: () => void
 }
 
+// most of these are from HeroIcons, https://heroicons.com/
 export const Lock = ({className, onClick}: IconProps) => (
   <svg xmlns="http://www.w3.org/2000/svg" onClick={onClick} className={`h-5 w-5 ${className}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import {Lock} from 'icons/Icons'
+import {LinkedIn, Instagram, Github} from 'icons/Social'
 import {Modal} from 'components/Modal/Modal'
 import {useModal} from 'components/Modal/Modal.hooks'
 import {Button} from 'components/Button/Button'
@@ -18,13 +18,18 @@ export default function Home() {
 
       <BreakpointHelper/>
 
-      <main className="flex flex-col flex-1 items-center justify-start w-full px-10 xs:px-20 mt-20 text-drkr-black">
-        <div className="max-w-md">
-          <img className="rounded-full border-8 border-drkr-black" src="/me4.jpg"/>
+      <main className="flex flex-col flex-1 items-center justify-start w-full px-10 xs:px-20 mt-10 xs:mt-20 text-drkr-black">
+        <div className="max-w-xxs xs:max-w-xs">
+          <img className="rounded-full border-4 border-drkr-black" src="/me4.jpg"/>
         </div>
         <div className="mt-4 sm:mt-8 text-center text-drkr-black">
-          <h1 className="text-4xl xs:text-5xl sm:text-6xl headline">andy dierker</h1>
-          <h2 className="text-1xl xs:text-2xl body mt-4">frontend software type of guy</h2>
+          <h1 className="text-4xl xs:text-5xl sm:text-6xl headline">Andy Dierker</h1>
+          <h2 className="text-2xl xs:text-2xl body mt-2 xs:mt-4">frontend software type of guy</h2>
+        </div>
+        <div className="flex flex-row justify-between w-1/2 max-w-xxs min-w-max mt-6">
+          <Github className="h-8 w-8 xs:h-10 xs:w-10 flex-1" url="https://github.com/adierker"/>
+          <LinkedIn className="h-8 w-8 xs:h-10 xs:w-10 flex-1" url="https://www.linkedin.com/in/dierker/"/>
+          <Instagram className="h-8 w-8 xs:h-10 xs:w-10 flex-1" url="https://www.instagram.com/dierker/"/>
         </div>
       </main>
 
