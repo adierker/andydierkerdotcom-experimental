3271bace1d41451222de70321df07014902c02e2
Andy Dierker <dierker@gmail.com>
2021-09-05 02:04:24 -0700

    WIP: added yup, started on reusable inputs, total nightmare right now, lots and lots of mistakes and bugs

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,27 +1,41 @@
+import {forwardRef} from 'react'
+
 interface InputProps {
+  id: string
   defaultValue?: any
   labelText?: string
   className?: string
   labelClassname?: string
+  error?: string
+  inputRef: any
 }
 
-export const Input = ({
-  defaultValue = '',
+export const Input = forwardRef<HTMLInputElement, InputProps>(({
+  id,
+  defaultValue,
   labelText,
-  className = '',
-  labelClassname = '',
-}: InputProps) => {
-  return (
-    <>
-      {labelText && (
-        <label className={`text-2xl headline-font mr-3 ${labelClassname}`}>
-          {labelText}
-        </label>
-      )}
-      <input 
-        defaultValue={defaultValue}
-        className={`drkr-focus border border-drkr-mid-gray border-2 h-10 px-2 focus-visible:bg-drkr-white focus-visible:border-drkr-black ${className}`}
-      />
-    </>
-  )
-}
+  className,
+  labelClassname,
+  error,
+  ...otherInputElementProps
+}: InputProps, ref) => (
+  <>
+    {labelText && (
+      <label 
+        htmlFor={id} 
+        className={`text-xl headline-spaced-font mr-3 ml-1 mb-1 block ${labelClassname}`}
+      >
+        {labelText}
+      </label>
+    )}
+    <input 
+      id={id}
+      ref={ref}
+      defaultValue={defaultValue}
+      autoComplete="off"
+      className={`drkr-focus border border-drkr-mid-gray border-2 h-10 px-2 mb-4 focus-visible:bg-drkr-white focus-visible:border-drkr-black body-font ${className}`}
+      {...otherInputElementProps}
+    />
+    {error && <div>error!</div>}
+  </>
+))
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,12 @@
-import {PageWrapper, Input} from 'components'
 import {useForm} from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+import {PageWrapper, Input} from 'components'
+
+const formSchema = yup.object().shape({
+  name: yup.string().required('Required!')
+})
 
 export const Admin = () => {
   const {
@@ -7,17 +14,28 @@ export const Admin = () => {
     handleSubmit,
     watch,
     formState: { errors } 
-  } = useForm()
+  } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(formSchema)
+  })
   const onSubmit = data => console.log(data)
 
+  const recipeName = register('name')
+  
   return (
     <PageWrapper pageTitle="andydierker.com | admin" hasHeader={true}>
       
-      <main className="flex flex-col flex-1 items-center justify-start w-full px-10 xs:px-20 py-10 text-drkr-black">
+      <main className="flex flex-col items-center justify-center px-10 xs:px-20 py-10 text-drkr-black">
         
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input labelText="name" className="w-full"/>   
+        <form className="w-1/2" onSubmit={handleSubmit(onSubmit)}>
+          <Input 
+            id="name"
+            labelText="Recipe Name"
+            className="w-full"
+            inputRef={recipeName.ref}
+          />
         </form>
+        {/* {errors && <div>{errors}</div>} */}
 
       </main>
 
