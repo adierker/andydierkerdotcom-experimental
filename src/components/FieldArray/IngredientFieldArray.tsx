c3d7f06cc5a33c7078cc97cf97204c4a07a48d39
Andy Dierker <dierker@gmail.com>
2021-09-14 23:56:00 -0700

    WIP clean up styles

--- src/components/FieldArray/IngredientFieldArray.tsx
+++ src/components/FieldArray/IngredientFieldArray.tsx
@@ -34,67 +34,65 @@ export const IngredientFieldArray = ({
 
   // this is all still a mess and isn't working at all
   return (
-    <>
+    <div className="">
       {ingredientFields.map((field, index) => (
         <div key={field.id}>
 
-        <div className="flex flex-row">
-          <div className="flex-1">
-            <div className="flex flex-row">
-              <div className="ml-8 flex-1">
-                <Input
-                  id={`num-${index}`}
-                  label="Num"
-                  hideErrorSection={true}
-                  {...register(`ingredientGroupings.${nestIndex}.ingredients.${index}.num` as const)}
-                />
+          <div className="flex flex-row">
+            <div className="flex-1">
+              <div className="flex flex-row">
+                <div className="ml-8 flex-1">
+                  <Input
+                    id={`num-${index}`}
+                    label="Num"
+                    hideErrorSection={true}
+                    {...register(`ingredientGroupings.${nestIndex}.ingredients.${index}.num` as const)}
+                  />
+                </div>
+                <div className="flex-1 ml-2">
+                  <Input
+                    id={`unit-${index}`}
+                    label="Unit"
+                    hideErrorSection={true}
+                    {...register(`ingredientGroupings.${nestIndex}.ingredients.${index}.unit` as const)}
+                  />
+                </div>
               </div>
-              <div className="flex-1 ml-2">
-                <Input
-                  id={`unit-${index}`}
-                  label="Unit"
-                  hideErrorSection={true}
-                  {...register(`ingredientGroupings.${nestIndex}.ingredients.${index}.unit` as const)}
-                />
+              <div className="flex flex-row">
+                <div className="ml-8 flex-1">
+                  <Input
+                    id={`ingredient-${index}`}
+                    label="Ingredient"
+                    labelClassName="mt-2"
+                    // error={fieldErrors?.[index]?.[fieldKey]}
+                    // iconWrapperClassName={index !== 0 && 'mt-2'}
+                    {...register(`ingredientGroupings.${nestIndex}.ingredients.${index}.ingredient` as const)}
+                  />
+                </div>
               </div>
             </div>
-            <div className="flex flex-row">
-              <div className="ml-8 flex-1">
-                <Input
-                  id={`ingredient-${index}`}
-                  label="Ingredient"
-                  labelClassName="mt-2"
-                  // error={fieldErrors?.[index]?.[fieldKey]}
-                  // iconWrapperClassName={index !== 0 && 'mt-2'}
-                  {...register(`ingredientGroupings.${nestIndex}.ingredients.${index}.ingredient` as const)}
-                />
-              </div>
+            <div className="flex items-center">
+              <button
+                type="button"
+                className="ml-3 text-center drkr-focus text-drkr-hover cursor-pointer"
+                onClick={() => removeIngredient(index)}
+              >
+                <PlainX/>
+              </button>
             </div>
           </div>
-          <div className="flex items-center">
-            <button
-              type="button"
-              className="ml-3 text-center drkr-focus text-drkr-hover cursor-pointer"
-              onClick={() => removeIngredient(index)}
-            >
-              <PlainX/>
-            </button>
-          </div>
-        </div>
 
 
           {ingredientFields.length > 1 && <hr className="ml-8 border-t-3 border-drkr-mid-gray mt-3 mb-8"/>}
-
-
-          <Button
-            text="Add Ingredient"
-            onClick={() => appendIngredient({})}
-            className="focus-visible:bg-drkr-black"
-          />
-
         </div>
       ))}
-    
-    </>
+      <div className="flex">
+        <Button
+          text="Add Ingredient"
+          onClick={() => appendIngredient({})}
+          className="focus-visible:bg-drkr-black block ml-auto mr-8"
+        />
+      </div>
+    </div>
   )
 }
